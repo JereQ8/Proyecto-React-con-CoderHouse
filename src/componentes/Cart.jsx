6d1cd8c0d64d5b7e8609660fd8c01b3601f03bec
Jeremias Quinteros <jeremias8quinteros@gmail.com>
2022-09-23 20:02:44 -0300

    contexto

--- src/componentes/Cart.jsx
+++ src/componentes/Cart.jsx
@@ -1,11 +1,59 @@
 import React from 'react'
+import { useContext } from 'react'
+import CartContext from '../context/CartContext'
+
+
+const fragment= document.createDocumentFragment()
+
 
 function Cart() {
+
+  const { productosComprados } = useContext(CartContext)
+
   return (
     <div>
-        <h1> Cart </h1> 
+      <h1> Cart </h1>
+      <div style={{border:"2px solid", margin:"5%"}}>
+        <div style={{ display: "inline-block", margin: "5%" }}>
+          <h5>Nombre</h5>
+          <div id='nombre'> {productosComprados.length === 0
+            ? "No hay productos en el carrito aun"
+            : productosComprados.map((producto, index) => {
+              return <div key={index}> <p> {producto.nombre} </p> <br /> </div>
+            })
+          } </div>
+        </div>
+
+        <div style={{ display: "inline-block", margin: "5%" }}>
+          <h5>Cantidad</h5>
+          <div id='cantidad'> {productosComprados.length === 0
+            ? ""
+            : productosComprados.map((producto, index) => {
+              return <div key={index}> <p> {producto.cantidad} </p> <br /> </div>
+            })
+          } </div>
+        </div>
+
+        <div style={{ display: "inline-block", margin: "5%" }}>
+          <h5> Precio </h5>
+          <div id='precio'>
+            {productosComprados.length === 0
+              ? ""
+              : productosComprados.map((producto, index) => {
+                return <div key={index}> <p> {producto.precio} </p> <br /> </div>
+              })
+            } </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Cart
+
+
+
+// const p= document.createElement("p");
+//                 const br= document.createElement("br")
+//                 p.textContent=producto.precio;
+//                 fragment.appendChild(p, br)
--- src/componentes/ItemDetail.jsx
+++ src/componentes/ItemDetail.jsx
@@ -1,7 +1,8 @@
 import "../css/itemDetail.css"
 import ItemCount from "./ItemCount"
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { Link } from "react-router-dom"
+import CartContext from "../context/CartContext"
 
 
 const llevarAlCart= ()=>{
@@ -18,15 +19,32 @@ export function botonLlevarAlCart(){
 
 function ItemDetail({ nombre, colores, precio, cuotas, stock }) {
 
-  const [comprar, setComprar]= useState(false)
-
-  const sumarAlCarrito= (e)=>{
-    setComprar(true)
-    e.nativeEvent.path[1].innerHTML="" 
-  }
-
+  const [comprar, setComprar]= useState(false);
+  const {productosComprados, agregarProductoAlCarrito}= useContext(CartContext);
   
+  const sumarAlCarrito= (e)=>{
 
+    if(e.nativeEvent.path[2].querySelector("div").querySelector("div").querySelector("div").textContent== 0){
+      alert("Para comprar debe marcar la cantidad de productos que desea")
+    }
+    else{
+      const productoComprado= {
+        nombre: e.nativeEvent.path[2].querySelector("h4").textContent,
+        cantidad: parseInt(e.nativeEvent.path[2].querySelector("div").querySelector("div").querySelector("div").textContent),
+        precio: parseInt(e.nativeEvent.path[2].querySelectorAll("p")[1].textContent)*parseInt(e.nativeEvent.path[2].querySelector("div").querySelector("div").querySelector("div").textContent) 
+      }
+      
+      setComprar(true)
+      e.nativeEvent.path[1].innerHTML="";
+      agregarProductoAlCarrito(productoComprado);
+      console.log(productosComprados)
+      
+      
+      
+    }
+    
+    
+  }
   return (
     <div className='item-detail' >
       <h4>{nombre}</h4>
@@ -38,7 +56,7 @@ function ItemDetail({ nombre, colores, precio, cuotas, stock }) {
       : <ItemCount nombreProducto={nombre} stockProducto={stock} sumarAlCarrito={sumarAlCarrito}/> 
       }
       <div>
-      <button onClick={sumarAlCarrito} style={{"margin-top":"5%"}}> Comprar </button>
+      <button onClick={sumarAlCarrito} style={{"marginTop":"5%"}}> Comprar </button>
       </div>
     </div>
   )
