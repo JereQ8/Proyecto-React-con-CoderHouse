44ff42d0c40fad35ead68508276a93f492d5b64f
Jeremias Quinteros <jeremias8quinteros@gmail.com>
2023-01-30 20:12:16 -0300

    logo

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import CartWidget from './CartWidget'
 import { NavLink } from 'react-router-dom'
+import logoEcomm from '../imgs/eclogo.png'
 
 
 const NavBar = () => {
 
-const logo= 'https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456'
 
   return (
     <>
       <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand cont-logo" href="index.html"><img className='logoNetflix' src={logo} alt="Logo"/></a>
+          <a className="navbar-brand cont-logo" href="index.html"><img className='logoNetflix' src={logoEcomm} alt="Logo"/></a>
           <a href='index.html'>  </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
